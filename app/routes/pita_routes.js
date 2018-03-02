bc5dadc91c69ab6ec56bb6d420a459df6e1c531b
Daniel Buckle <daniel@mumsnet.com>
2018-03-02 12:47:42 +0000

    Save the user agent info

--- app/routes/pita_routes.js
+++ app/routes/pita_routes.js
@@ -1,6 +1,10 @@
-var ObjectID = require('mongodb').ObjectID;
+var ObjectID 	= require('mongodb').ObjectID;
+var useragent = require('express-useragent');
+var Report 		= require('../models/report.js');
 
 module.exports = function(app, db) {
+	app.use(useragent.express());
+
 	app.get('/pita/:id', (req, res)=> {
 		const id = req.params.id;
 		const details = { '_id': new ObjectID(id) };
@@ -15,17 +19,26 @@ module.exports = function(app, db) {
 
 	app.post('/pita', (req, res) => {
 		const pita = { text: req.body.body, title: req.body.title }
-    	db.collection('pita').insert(pita, (err, result)=> {
-    		if (err) {
-    			res.send({ 'error': 'An error has occured' });
-    		} else {
-    			res.send(result.ops[0]);
-    		}
-    	})
-  	});
+		var newReport = new Report(
+			{
+				browser: req.useragent.browser,
+				version: req.useragent.version,
+				os: req.useragent.os,
+				platform: req.useragent.platform,
+				reportText: req.body.report
+			}
+		);
+    db.collection('pita').insert(newReport, (err, result)=> {
+    	if (err) {
+    		res.send({ 'error': 'An error has occured' });
+    	} else {
+    		res.send(result.ops[0]);
+    	}
+    })
+  });
 
-  	app.delete('/pita/:id', (req, res) => {
-  		const id = req.params.id;
+  app.delete('/pita/:id', (req, res) => {
+  	const id = req.params.id;
 		const details = { '_id': new ObjectID(id) };
 		db.collection('pita').remove(details, (err, item) => {
 			if (err) {
@@ -34,10 +47,10 @@ module.exports = function(app, db) {
 				res.send('Pita ' + id + ' deleted!');
 			}
 		});
-  	});
+  });
 
-  	app.put('/pita/:id', (req, res) => {
-  		const id = req.params.id;
+  app.put('/pita/:id', (req, res) => {
+  	const id = req.params.id;
 		const details = { '_id': new ObjectID(id) };
 		const pita = { text: req.body.body, title: req.body.title }
 		db.collection('pita').update(details, pita, (err, result) => {
@@ -47,5 +60,5 @@ module.exports = function(app, db) {
 				res.send(pita);
 			}
 		});
-  	});
+  });
 };
