7efe9830d5f4305baa4b019d9b8d89f7342560bf
Daniel Buckle <daniel@mumsnet.com>
2018-03-26 10:51:04 +0100

    Fix Docker

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
 exports.port = () => process.env.PORT || 8000;
-exports.dbUrl = () => process.env.DB_URL || "mongodb://localhost:27017"
+exports.dbUrl = () => process.env.DB_URL || "mongodb://mongodb:27017"
